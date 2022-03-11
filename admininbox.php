6ae81a5ac6b3fef30af6013a5e3206e9f363c389
raycianavera <raycianavera@gmail.com>
2022-03-12 01:49:29 +0800

    updated with search feature on Admin inbox

--- admininbox.php
+++ admininbox.php
@@ -60,9 +60,9 @@
 
 
 
-          <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="log-button search-btn my-2 my-sm-0" type="submit">Search</button>
+          <form class="form-inline" action="includes/search.php" method="POST">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="input-search">
+            <button class="log-button search-btn my-2 my-sm-0" type="submit" name="submit-search">Search</button>
           </form>
 
         </div>
